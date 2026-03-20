40f9c07318aea72213ef8652be6f9ad4f61d8cff
koniqtech <sharad.startalpha@gmail.com>
2026-03-20 12:20:58 +0530

    added start script

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello GitHub!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Hostinger 🚀");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});

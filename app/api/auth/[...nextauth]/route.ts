9d712aa961482d4869a234953de19eeb38cee699
koniqtech <sharad.startalpha@gmail.com>
2026-03-21 17:39:42 +0530

    fix nextauth secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,14 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
 
 const handler = NextAuth({
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-});
+  secret: process.env.NEXTAUTH_SECRET, // ✅ REQUIRED
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }

40f38ab51126fb3dbb97ac4c3f75ed55eb99e1e9
koniqtech <sharad.startalpha@gmail.com>
2026-04-03 16:06:00 +0530

    final deploy

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,15 +2,12 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex items-center">
-      <Image
-        src="/logo.png"
-        alt="KoniqTech"
-        width={120}
-        height={40}
-        priority
-        className="object-contain"
-      />
-    </div>
+    <Image
+      src="/logo.svg"
+      alt="KoniqTech"
+      width={120}
+      height={40}
+      priority
+    />
   );
 }

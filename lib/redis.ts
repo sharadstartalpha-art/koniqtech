b4dfb43b7ccc00c0db21bd5e1f7f859d270bc4f8
koniqtech <sharad.startalpha@gmail.com>
2026-04-16 20:50:49 +0530

    fix: Final CRM  Ui

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,7 +1,9 @@
 import { Redis } from "ioredis";
 
-const isBuild = process.env.NEXT_PHASE === "phase-production-build";
+export const redis = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
 
-export const redis = isBuild
-  ? ({} as any) // 🚫 disable during build
-  : new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
+  // ✅ ADD THIS FOR UPSTASH
+  tls: {},
+});

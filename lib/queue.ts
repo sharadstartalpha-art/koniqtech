af98c17464ddfbc859c7a095d9550fd29280f555
koniqtech <sharad.startalpha@gmail.com>
2026-04-16 21:03:16 +0530

    fix: Final CRM  Ui

--- lib/queue.ts
+++ lib/queue.ts
@@ -3,4 +3,13 @@ import { redis } from "./redis";
 
 export const campaignQueue = new Queue("campaign-queue", {
   connection: redis,
+
+  // 🔥 RATE LIMIT
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: {
+      type: "exponential",
+      delay: 5000,
+    },
+  },
 });

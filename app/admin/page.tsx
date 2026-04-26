c7034398ce2e1d1327da216083a7485730016546
koniqtech <sharad.startalpha@gmail.com>
2026-04-27 01:39:43 +0530

    fix generate api

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,7 +10,7 @@ export default async function AdminPage() {
 
       <div className="grid grid-cols-2 gap-4 mt-6">
         <div className="p-4 border">Users: {users}</div>
-        <div className="p-4 border">Invoices: {invoices}</div>
+        <div className="p-4 border">Invoices New: {invoices}</div>
       </div>
     </div>
   );

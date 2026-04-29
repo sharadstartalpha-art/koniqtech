58ff75bf6e1a7d34faaa75054005b1933f03c313
koniqtech <sharad.startalpha@gmail.com>
2026-04-30 02:16:06 +0530

    fix products api route

---
+++ app/api/products/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+/* GET PRODUCTS */
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany();
+
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error("PRODUCT ERROR:", error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch products" },
+      { status: 500 }
+    );
+  }
+}

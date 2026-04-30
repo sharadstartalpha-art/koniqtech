141182e9a129372747cf185022a4c842f3972241
koniqtech <sharad.startalpha@gmail.com>
2026-04-30 16:29:31 +0530

    fix products api route

--- app/api/products/[slug]/route.ts
+++ app/api/products/[slug]/route.ts
@@ -2,11 +2,13 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic"; // 🔥 FIX CACHE
+
 export async function GET(
   req: NextRequest,
-  context: { params: Promise<{ slug: string }> } // ✅ FIXED
+  context: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = await context.params; // ✅ MUST await
+  const { slug } = await context.params;
 
   const product = await prisma.product.findUnique({
     where: { slug },

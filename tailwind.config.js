1b590664d2c82cfdb0350c847bf8cb39cb52f9ed
koniqtech <sharad.startalpha@gmail.com>
2026-05-19 23:35:34 +0530

    frontend production ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#2563eb", // blue
-        accent: "#10b981",  // green
-      },
-    },
-  },
-  plugins: [],
-};
+
+content:[
+
+"./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+"./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+"./modules/**/*.{js,ts,jsx,tsx,mdx}"
+
+],
+
+theme:{
+
+extend:{}
+
+},
+
+plugins:[]
+
+}

f6b53fd64a77634e527ca6400aa653950754708c
koniqtech <sharad.startalpha@gmail.com>
2026-07-01 16:21:50 +0530

    add superadmin page

--- shared/components/admin/AdminHeader.tsx
+++ shared/components/admin/AdminHeader.tsx
@@ -15,6 +15,7 @@ import {
   Moon,
   Sun,
   Command,
+  Bell,
 } from "lucide-react"
 
 import NotificationDropdown from "./NotificationDropdown"
@@ -113,16 +114,93 @@ export default function AdminHeader() {
 
         <div className="flex items-center gap-2">
 
-          <NotificationDropdown
-            open={notificationOpen}
-            setOpen={setNotificationOpen}
-          />
+         <div className="relative">
 
-         <UserDropdown
-    open={userOpen}
-    setOpen={setUserOpen}
-    user={user}
-/>
+    <button
+
+        onClick={() =>
+            setNotificationOpen(!notificationOpen)
+        }
+
+        className="
+        h-11
+        w-11
+        rounded-xl
+        border
+        flex
+        items-center
+        justify-center
+        hover:bg-slate-100
+        "
+
+    >
+
+        <Bell size={18} />
+
+    </button>
+
+    <NotificationDropdown
+        open={notificationOpen}
+        setOpen={setNotificationOpen}
+    />
+
+</div>
+
+        <div className="relative">
+
+    <button
+        onClick={() => setUserOpen(!userOpen)}
+        className="
+        flex
+        items-center
+        gap-3
+        rounded-xl
+        border
+        px-3
+        py-2
+        hover:bg-slate-50
+        "
+    >
+
+        <div
+            className="
+            h-10
+            w-10
+            rounded-full
+            bg-orange-600
+            text-white
+            flex
+            items-center
+            justify-center
+            font-semibold
+            "
+        >
+            {user.name?.charAt(0).toUpperCase() ?? "A"}
+        </div>
+
+        <div className="text-left">
+
+            <div className="font-semibold">
+                {user.name}
+            </div>
+
+            <div className="text-xs text-slate-500">
+                {user.email}
+            </div>
+
+        </div>
+
+    </button>
+
+    <UserDropdown
+        open={userOpen}
+        setOpen={setUserOpen}
+        user={user}
+    />
+
+</div>
+
+
 
         </div>
 

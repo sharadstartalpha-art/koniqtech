1d816d23fac9e60631f98ecbda2f4066302aa763
koniqtech <sharad.startalpha@gmail.com>
2026-08-17 11:20:38 +0530

    fix demo data generation actions

--- app/(app)/settings/company/page.tsx
+++ app/(app)/settings/company/page.tsx
@@ -2,15 +2,26 @@ import { auth } from "@/auth";
 import prisma from "@/shared/lib/prisma";
 
 export default async function CompanyPage() {
-  try {
-    const session = await auth();
+  const session = await auth();
 
-    return (
-      <pre className="p-10">
-        {JSON.stringify(session, null, 2)}
-      </pre>
-    );
-  } catch (e) {
-    return <pre>{String(e)}</pre>;
-  }
+  const orgId = (session?.user as any).orgId;
+
+  const organization = await prisma.organization.findUnique({
+    where: {
+      id: orgId,
+    },
+  });
+
+  return (
+    <pre>
+      {JSON.stringify(
+        {
+          orgId,
+          organization,
+        },
+        null,
+        2
+      )}
+    </pre>
+  );
 }
